de9c888ff6cf4dbe2cf6e5cb01850ce7f37847b7
Anus Younus <AnusYounus254@gmail.com>
2024-03-04 18:45:49 +0500

    added class 3

---
+++ class 3/app.js
@@ -0,0 +1,57 @@
+// let userName = prompt("Enter your name");
+
+// let num1 = +prompt("Enter your num1");
+// let num2 = +prompt("Enter your num2");
+
+// console.log("Welcome" + userName + "Your total is:  " + (num1 + num2));
+
+// let bullet = 1;
+
+// Pre Increment
+
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet++);
+// console.log(bullet)
+// console.log(bullet)
+// console.log(bullet)
+
+// Pre Decrement
+
+// let number = 1;
+
+// console.log(++number)
+// console.log(++number)
+// console.log(number)
+// console.log(number)
+
+// Post Increment
+
+// let number = 10;
+
+// console.log(number--)
+// console.log(number--)
+// console.log(number--)
+// console.log(number--)
+// console.log(number--)
+// console.log(number--)
+// console.log(number)
+// console.log(number)
+// console.log(number)
+
+// Post Decrement
+
+// console.log(--number);
+// console.log(--number);
+// console.log(--number);
+// console.log(--number);
+// console.log(number);
+// console.log(number);
+// console.log(number);
+
+
+
