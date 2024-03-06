7f7aa09172187a7a824b3ba4432ca700f8c58aa9
Anus Younus <AnusYounus254@gmail.com>
2024-03-06 18:41:23 +0500

    added class 4

---
+++ class 4/app.js
@@ -0,0 +1,96 @@
+// const bYear = prompt("enter your age");
+
+// let num1 = +prompt("enter a number")
+// let num2 = +prompt("enter another number")
+
+// prompt('')
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+/*
+let job = prompt("enter your job");
+
+let country = prompt("enter your location");
+let lifePartner = prompt("enter your lifePartner");
+
+let child = prompt("enter your children");
+
+let message =
+  "you will be a " +
+  job +
+  " in" +
+  country +
+  " and married to" +
+  lifePartner +
+  " with" +
+  child +
+  " kids";
+
+console.log(message);
+alert(message);
+document.write(message); */
+
+/*
+let email = prompt("enter your email")
+
+let message = ('My email address is ' +  email)
+
+console.log(message)
+
+alert(message)
+
+document.write(message) */
+
+/*
+let age = prompt("enter your age")
+
+let message = ('I am  ' +  age + ' years old')
+
+console.log(message)
+
+alert(message)
+
+document.write(message) */
+
+/*
+let str = 5;
+let num = "5";
+
+console.log(typeof num)
+console.log(typeof str)*/
+
+/*
+let birthYear = prompt("enter your birth year")
+
+let num = 2
+
+document.write('my birth year is ' + birthYear + '<br />' + 'Data of my deceleared varriable is ' + typeof num)*/
+/*
+let userName = prompt("enter your name");
+
+let quantity = prompt("enter your quantity");
+
+let item = prompt("enter your item");
+
+const price = 500;
+
+let total = quantity * price;
+
+document.write(userName + ' orderd ' + quantity + item + ' from abc store of ' +  price + ' each his total is '
+ + total)*/
+
+/*
+ let num1 = +prompt('enter your num1')
+ let num2 = +prompt('enter your num2')
+
+ let sum  = (num1 + num2)
+ let subtract  = num1 - num2
+ let mul  = num1 * num2
+ let divide  = num1 / num2
+ let reminder  = num1 % num2
+
+ alert('your sum ' + num1 + ' and ' + num2 + ' is ' + sum)
+ alert('your subtract ' + num1  + ' and ' + num2 + ' is ' + subtract)
+ alert('your multiplay ' + num1 + ' and ' + num2 + ' is ' + mul)
+ alert('your divide ' + num1 + ' and ' + num2 + ' is ' + divide)
+ alert('your reminder ' + num1 + ' and ' + num2 + ' is ' + reminder)
+ */
