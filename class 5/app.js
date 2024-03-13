85ad7ad36a850894e32e9aee094995a929b3d9b7
Anus Younus <AnusYounus254@gmail.com>
2024-03-13 15:19:47 +0500

    maked shoes website

---
+++ class 5/app.js
@@ -0,0 +1,61 @@
+// let isFarhanShopOpen = confirm("is farhan shop open");
+
+// if (isFarhanShopOpen) {
+//   console.log("bring french fries");
+// }
+// else{
+//     console.log('drink some thing to eat')
+// }
+
+
+// let shopName = prompt('which shop is open?')
+
+// if(shopName === 'farhan'){
+//     console.log('bring french fries');
+// }
+// else if(shopName === 'almadina'){
+//     console.log('bring one burger')
+// }
+// else if(shopName === 'ejaaz'){
+//     console.log('bring 1 biryani')
+// }
+// else {
+//     console.log('bring some thing to eat')
+// }
+
+// let bodyFat = prompt('Enter your body fat percentage')
+
+// if(bodyFat >= 40){
+//     console.log('cardio')
+// }
+// else if(bodyFat >= 30){
+//     console.log('strength')
+// }
+// else if(bodyFat >= 20){
+//     console.log('cardio and strenth')
+// }
+// else if(bodyFat < 20){
+//     console.log('crossfit')
+// }
+
+// let isMatricullate = confirm('Are you matriculate')
+// let score = prompt('what was your entry test score')
+
+// if (isMatricullate && score >= 60){
+//     console.log('congrats! you have got admission');
+// }
+
+// let weight = +prompt('Enter your weight')
+// let runingTime = +prompt('Enter your runing time')
+// let gender = prompt('Enter your gender')
+// let age = +prompt('Enter your age')
+
+// if (weight >= 300 && runingTime < 6 && gender === 'male' && age >= 30){
+//     console.log('You are eligible');
+// }
+
+let avalibleFood = prompt('which food is avalible')
+
+if (avalibleFood === 'french fries' || avalibleFood === 'burger' || avalibleFood === 'samosa'){
+    console.log('bring 1');
+}
---
+++ class 6/app.js
@@ -0,0 +1,7 @@
+// let city = prompt('Enter Your city')
+// if(city === 'Karachi'){
+//     alert('Welcome to the city of light')
+//     console.log('Welcome to the city of light')
+//     document.write('Welcome to the city of light')
+// }
+
