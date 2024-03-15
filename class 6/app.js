2fccbea4ea1a7624b56f30dd7b261a7850263c92
Anus Younus <AnusYounus254@gmail.com>
2024-03-15 19:09:48 +0500

    added class 7

--- class 6/app.js
+++ class 6/app.js
@@ -5,17 +5,80 @@
 //     document.write('Welcome to the city of light')
 // }
 
-let gender = prompt('Enter your gender')
+// let gender = prompt('Enter your gender')
 
 
-if(gender === 'Male'){
-    console.log('Welcome Sir')
-    document.write('Welcome Sir')
-    alert('Welcome Sir')
-}
-else if(gender === 'Female'){
-    console.log('Welcome Mam')
-    document.write('Welcome Mam')
-    alert('Welcome Mam')
-}
+// if(gender === 'Male'){
+//     console.log('Welcome Sir')
+//     document.write('Welcome Sir')
+//     alert('Welcome Sir')
+// }
+// else if(gender === 'Female'){
+//     console.log('Welcome Mam')
+//     document.write('Welcome Mam')
+//     alert('Welcome Mam')
+// }
+
+
+// let traficLight = prompt('Enter which light color')
+// if(traficLight === 'red'){
+//     console.log('Vehicals must stops');
+//     document.write('Vehicals must stops');
+//     alert('Vehicals must stops');
+// }
+// else if(traficLight === 'yellow'){
+//     console.log('Vehicals should ready to move');
+//     document.write('Vehicals should ready to move');
+//     alert('Vehicals should ready to move');
+    
+// }
+// else if(traficLight === 'green'){
+//     console.log('Vehicals can move now');
+//     document.write('Vehicals can move now');
+//     alert('Vehicals can move now');
+   
+// }
+
+// let areYouGraduate = confirm('Are you Graduate')
+// let division = +prompt('What was your score')
+// let sports = prompt('which sports do you play')
+
+// if((areYouGraduate && division >= 60) || sports === 'circket'){
+//     console.log('you are eligible for admission')
+// }
+// let areYouGraduate = confirm('Are you Graduate')
+// let sports = prompt('your sports name')
+// if (areYouGraduate || sports === 'circket'){
+//     if(areYouGraduate && +prompt('what was your score') >= 60){
+//         console.log('You are eligible')
+//     }
+// }
+
+// let totalMarks = prompt('Enter your total marks')
+// let obtainMarks = prompt('Enter your obtain marks')
+// let persentage = obtainMarks*totalMarks/100
+// let grade, remarks
+
+
+//     document.write('Total Marks :' + totalMarks)
+//     document.write("<br />")
+//     document.write('Obtain: ' + obtainMarks)
+//     document.write("<br />")
+//     document.write('Perstage:' + persentage)
+//     document.write("<br />")
+//    if (persentage >= 80){
+//     grade = 'A-1', remarks = 'You are good'
+//    }
+//    else if (persentage >= 70){
+//     grade = 'A', remarks = 'You are good'
+//    }
+//    else if (persentage >= 60){
+//     grade = 'B', remarks = 'Need to improve'
+//    } 
+//   else if (persentage >= 80){
+//     grade = 'C', remarks = ',Moye Moye'
+//    }
+
+
+let user = prompt("Enter Your Number")
 
---
+++ class 7/app.js
@@ -0,0 +1,99 @@
+// const num1 = +prompt("Enter your num1")
+// const num2 = +prompt("Enter your num2")
+// const operator = prompt("Enter your operator")
+
+// if(operator == "+"){
+//     console.log(num1+num2)
+
+// }
+
+// if(operator == "-"){
+//     console.log(num1-num2)
+
+// }
+
+// if(operator == "/"){
+//     console.log(num1/num2)
+
+// }
+
+// if(operator == "*"){
+//     console.log(num1*num2)
+
+// }
+
+// if(operator == "%"){
+//     console.log(num1*num2/100)
+
+// }
+// const chr = prompt('enter a character')
+// if(chr === "a" ||
+//     chr === "e" ||
+//     chr === "i" ||
+//     chr === "o"||
+//     chr === "u" ){
+//       console.log('your charcter is vowel');
+// }
+// else{
+//     console.log('Not a vowel')
+// }
+
+// let areYouGraduate = confirm("Are you Graduate");
+// if (areYouGraduate) {
+//     let faculty = prompt('Which faculty you were enrolled in?')
+//     if(faculty === "engineer"){
+//   console.log("You are appointed to engineer");
+//     }
+//     else if(faculty === "medical"){
+//        console.log("You are appointed to Doctor");
+//     }
+//     else if(faculty === 'commerce'){
+//        let interest = prompt('are you interested in HR or accounts')
+//        if(interest === "HR"){
+//          console.log("You are appointed to HR");
+//        }
+//        else if(interest === "accounts"){
+//        console.log("You are appointed to accounts");
+//        }
+//     }
+// } else {
+//   console.log("You are appointed to peun");
+// }
+
+// let a = 10;
+// let b = 10;
+// let c = 10;
+
+// if(a==b==c){
+//     console.log('Hello')
+// }
+// else{
+//     console.log('Good By')
+// }
+
+// if("car" < "cat") console.log('True')
+
+// Array
+
+// let students = ["Anus", "Manan", "Zian", "Mustaqeem"]
+
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+
+// let cars = ["Aulto", "Corola", "Dala", "Mehran"]
+
+// console.log(cars[0])
+// console.log(cars[1])
+// console.log(cars[2])
+// console.log(cars[3])
+
+// let students = [];
+
+// students[0] = "Ahmad"
+// students[1] = "Anas"
+// students[5] = "Manan"
+// console.log(students[3])
+
+let arr = ["Anus", "Owais"];
