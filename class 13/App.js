a177f775565b8726f69ebcd080595765ebf61e67
Anus Younus <AnusYounus254@gmail.com>
2024-04-03 21:25:06 +0500

    Added class 14

---
+++ class 13/App.js
@@ -0,0 +1,21 @@
+// let arr = [1,2,3,4,5,6,7,8,9]
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+// var primeNumbe = [1, 2, 3, 4, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+
+// var favArr = primeNumbe.slice(5, 9);
+
+// console.log(favArr);
+
+// let primeNumber = [1, 2, 3, 4, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+// let favNum = []
+// for (let i = 0; i < primeNumber.length; i++) {
+//     if (primeNumber[i] >= 10 && primeNumber[i] <= 20) {
+//       favNum.push(primeNumber[i])
+//     }
+// }
+
+// console.log(favNum);
+
---
+++ class 14/App.js
@@ -0,0 +1,37 @@
+// let userName = prompt("Enter your Name")
+
+// let firtChr = userName[0]
+
+// firtChr = firtChr.toUpperCase()
+
+// let lastChr = userName.slice(1)
+
+// lastChr = lastChr.toLowerCase()
+
+// console.log(firtChr + lastChr);
+
+// let fruits = ['orange','banana','watermelon','apple']
+
+// console.log(fruits.sort());
+
+// const points = [40, 100, 1, 5, 25, 10];
+// points.toSorted(function(a, b){return a-b});
+
+// console.log(points);
+
+// let fruits = ['banana', 'apple', 'watermelon', 'orange']
+
+// let sortedFruits = fruits.toSorted()
+// let reversFruits = fruits.toReversed()
+
+// console.log(fruits, sortedFruits, reversFruits);
+
+let num = 2.5
+
+let round = Math.round(num)
+
+let ceiledValue = Math.ceil(num)
+
+let flooredValue = Math.floor(num)
+
+console.log(round, ceiledValue, flooredValue);
