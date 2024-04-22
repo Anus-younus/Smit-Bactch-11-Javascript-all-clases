2a1cf852b5d4716435a047a975c1bd66dc9673c3
Anus Younus <AnusYounus254@gmail.com>
2024-04-22 21:47:03 +0500

    added class 18

---
+++ class 18/app.js
@@ -0,0 +1,198 @@
+// let str = "the quick world war II brown fox jumps world war II over the lazy world war II dog.";
+
+// str = str.replace(/world war II/g, "the second world war")
+
+// console.log(str);
+
+// let num = "55555555"
+
+// num = parseInt(num)
+
+// console.log(typeof num);
+
+// let num = "5.34213343"
+
+// // num = parseInt(num)
+// num = parseFloat(num);
+
+// console.log(num);
+
+// let num = "123456789"
+
+// num = Number(num)
+
+// console.log(num);
+
+// let num = "apple"
+
+// num = Number(num);
+
+// console.log(isNaN(num));
+
+// let str = 12345667
+
+// str = str.toString()
+
+// console.log(str.split());
+
+// let bill = 2.434556
+
+// bill = bill.toFixed(2)
+
+// console.log(bill);
+
+// let num = "2548633110"
+
+// console.log(num.lastIndexOf('3'));
+
+// console.log(num.indexOf('3'));
+
+// let currentDate = new Date()
+
+// let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Tharsday', 'Friday', 'Saturday']
+
+// let currentDay = currentDate.getDay()
+
+// console.log(days[currentDay]);
+
+// let Months =  ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+// let currentMonth = currentDate.getMonth()
+
+// console.log(Months[currentMonth]);
+
+// let date = currentDate.getDate()
+
+// console.log(date);
+
+// let currentYear = currentDate.getFullYear()
+
+// console.log(currentYear);
+
+// let currentHours = currentDate.getHours()
+
+// console.log(currentHours);
+
+// let currentMinutes = currentDate.getMinutes()
+
+// console.log(currentMinutes);
+
+// let currentSeconds = currentDate.getSeconds()
+
+// console.log(currentSeconds);
+
+// let currentMiliSeconds = currentDate.getMilliseconds()
+
+// console.log(currentMiliSeconds);
+
+// let currentDate = new Date("April 15, 2005")
+
+// console.log(currentDate);
+
+// let now = new Date()
+
+// let age = now.getTime() - currentDate.getTime()
+
+// console.log(age/1000/60/60/24/365.25);
+
+// let num = "223344551123"
+
+// num = parseInt(num)
+
+// console.log(num);
+
+// let numEle = "12121212.1212212"
+
+// numEle = parseFloat(numEle)
+
+// console.log(numEle);
+
+// let myNumber = "1122334556677"
+
+// myNumber = Number(myNumber);
+
+// console.log(myNumber);
+
+// let str = 11223344455
+
+// str = str.toString()
+
+// console.log(str.split(" "));
+
+// let myStr = 1.2323
+
+// console.log(myStr.toFixed(2));
+
+
+// let array = [1,2,3,4,5,3,6,7]
+
+// console.log(array.lastIndexOf(3));
+
+// let date = new Date()
+
+// let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Tharsday', 'Friday', 'Saturday']
+
+// let currentDay = date.getDay()
+
+// console.log(days[currentDay]);
+
+// let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+// let currentMonth = date.getMonth()
+
+// console.log(months[currentMonth]);
+
+// let currentDate = date.getDate()
+
+// console.log(currentDate);
+
+// let currentHours = date.getHours()
+
+// console.log(currentHours);
+
+// let currentMinutes = date.getMinutes()
+
+// console.log(currentMinutes);
+
+// let currentSeconds = date.getSeconds()
+
+// console.log(currentSeconds);
+
+// let currentMiliSeconds = date.getMilliseconds()
+
+// console.log(currentMiliSeconds);
+
+// let dob = new Date("April 15, 2005")
+
+// let currentDate = new Date()
+
+// let age = currentDate.getTime() - dob.getTime()
+
+// console.log(Math.floor(age/1000/60/60/24/365.25));
+
+// for (var i = 0; i < text.length; i++) {
+// if (text.slice(i, i + 12) === "World War II") {
+// text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+// }
+// }
+
+// var firstChar = text.indexOf("World War II");
+
+// var firstChar = text.indexOf("World War II");
+// if (firstChar !== -1) {
+// text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// }
+
+let str = "the quick world war II brown fox jumps world war II over the lazy world war II dog.";
+
+console.log(str);
+
+let txt = str.indexOf("world war II")
+
+if(txt !== -1){
+    str = str.slice(0, txt) + "the second world war" + str.slice(txt + 12)
+}
+console.log(str);
+
+var text = "To be or not to be.";
+var segIndex = text.lastIndexOf("be");
