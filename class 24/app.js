914fbd2efadcf68b3dd8f19a7dfa14681b2c140a
Anus Younus <AnusYounus254@gmail.com>
2024-05-03 22:55:00 +0500

    added class 22 23 and 24

---
+++ class 24/app.js
@@ -0,0 +1,50 @@
+//Local variable
+// function sqr(num){
+//     let sqr = num * num
+//     return sqr
+// }
+
+// console.log(sqr(2));
+
+// //Global variable
+// var userName = "Anus"
+// var email = "abc@gmail.com"
+
+// function abc(){
+//     userName = "Owais"
+//     var email = "xyz@gmail.com"
+//     console.log(userName + email);
+// }
+
+// console.log(userName + email);
+// abc()
+// console.log(userName + email);
+
+// //if Block variable
+
+// var firstName = "Anus"
+// var fatherName = "Younus"
+
+// function xyz(){
+//     firstName = "Manan"
+//     if(true){
+//         let lastName = "Arif"
+//     }
+//     console.log(firstName + lastName);
+// }
+
+// xyz()
+
+function createBook(bookName, author, pages, topic) {
+    var book = {
+        bookName : bookName,
+        author : author,
+        pages : pages,
+        topic : topic
+    }
+    return book;
+}
+
+let book1 = createBook("Bahar Shariat", "Mufti Amjad", 9000, "Islamic");
+
+console.log(book1);
